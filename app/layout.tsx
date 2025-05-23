34ba5042cd2c60fd566d79d11abbe982ea74320d
TheSamE03 <erneboys2010@gmail.com>
2025-05-23 12:24:03 -0400

    Added speed insights to app/layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "../components/Navbar";
 import { Providers } from "../components/Providers";
 import { Footer } from "../components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Footer />
         </Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
